fb582a1277ef7a408b2c5bae8f44ed50fd27c522
soyoon26 <barcardi26@gmail.com>
2024-11-08 21:30:21 +0900

    feat: 프로필 내용

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,8 +4,50 @@ import Image from "next/image";
 
 const page = () => {
   return (
-    <div className="bg-black h-screen w-screen">
-      <Image src={profilecut} alt="Profile" width={200} height={200} />
+    <div className="bg-black w-screen flex">
+      <Image src={profilecut} alt="Profile" width={300} height={200} />
+      <section>
+        <div className="flex-col">
+          <div className="flex">
+            <div>생일</div>
+            <div>1996년 2월 14일</div>
+          </div>
+          <div className="flex">
+            <div>혈액형</div>
+            <div>O형</div>
+          </div>
+          <div className="flex">
+            <div>키</div>
+            <div>182cm</div>
+          </div>
+          <div className="fles">
+            <div>가족</div>
+            <div className="flex-col">
+              <div>부모님, 남동생</div>
+              <div>반려견 원이, 코코, 레오나르도 디카프리오</div>
+            </div>
+          </div>
+          <div className="flex">
+            <div>학력</div>
+            <div className="flex-col">
+              <div>배문고등학교</div>
+              <div>한양대학교 예술·체육대학 연극영화학 </div>
+            </div>
+          </div>
+          <div className="flex">
+            <div>MBTI</div>
+            <div>INFJ</div>
+          </div>
+          <div className="flex">
+            <div>소속사</div>
+            <div>매니지먼트엠엠엠</div>
+          </div>
+          <div className="flex">
+            <div>데뷔</div>
+            <div>2017년 KBS 2TV 드라마 학교 2017</div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
