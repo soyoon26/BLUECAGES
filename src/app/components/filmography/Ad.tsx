9e41e518d147328ce144adeab086d128784fbaf4
soyoon26 <barcardi26@gmail.com>
2024-11-08 21:34:00 +0900

    feat: 필모그래피 카테고리 생성

---
+++ src/app/components/filmography/Ad.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Ad = () => {
+  return <div>Ad</div>;
+};
+
+export default Ad;
---
+++ src/app/components/filmography/Drama.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Drama = () => {
+  return <div>Drama</div>;
+};
+
+export default Drama;
