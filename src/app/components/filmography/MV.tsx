94f0a1d756d7f50adc787263aba528e17a29db6d
soyoon26 <barcardi26@gmail.com>
2024-11-09 11:46:58 +0900

    feat: mv map

--- src/app/components/filmography/MV.tsx
+++ src/app/components/filmography/MV.tsx
@@ -2,45 +2,86 @@
 
 import React, { useState } from "react";
 import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import mv2 from "../../../../public/태연뮤비.png";
+import mv1 from "../../../../public/옥탑방에서.png";
+import mv3 from "../../../../public/내일의우리.png";
+
+const videoData = [
+  {
+    id: 1,
+    src: mv1,
+    alt: "mv1",
+    title: "옥탑방에서 - 좋아서하는밴드",
+    content: "2017년 12월 5일",
+    videoUrl: "https://www.youtube.com/embed/9_9tBgbdMRk",
+  },
+  {
+    id: 2,
+    src: mv2,
+    alt: "mv2",
+    title: "Can't Control Myself - 태연",
+    content: "2022년 1월 17일",
+    videoUrl: "https://www.youtube.com/embed/RccDIpx4ZMM",
+  },
+
+  {
+    id: 3,
+    src: mv3,
+    alt: "mv3",
+    title: "내일의 우리 - 카더가든",
+    content: "2023년 9월 12일",
+    videoUrl: "https://www.youtube.com/embed/IxaTjGPP2aM",
+  },
+];
 
 const MV = () => {
-  const [isVideoVisible, setIsVideoVisible] = useState(false);
+  const [visibleVideoId, setVisibleVideoId] = useState(null);
 
-  const toggleVideoVisibility = () => {
-    setIsVideoVisible(!isVideoVisible);
+  const toggleVideoVisibility = (id) => {
+    setVisibleVideoId(visibleVideoId === id ? null : id);
   };
 
   return (
     <div>
-      <section
-        onClick={toggleVideoVisibility}
-        className="cursor-pointer p-4 text-center"
-      >
-        {isVideoVisible ? (
-          <div>
-            <p>Can't Control Myself - 태연</p>
-            <ChevronUpIcon className="w-6 h-6" />
-          </div>
-        ) : (
-          <div>
-            <p>Can't Control Myself - 태연</p>
-            <ChevronDownIcon className="w-6 h-6" />
-          </div>
-        )}
-      </section>
-      {isVideoVisible && (
-        <div className="video-container mt-4">
-          <iframe
-            width="1000"
-            height="515"
-            src="https://www.youtube.com/embed/RccDIpx4ZMM"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+      {videoData.map((video) => (
+        <div key={video.id}>
+          <section
+            onClick={() => toggleVideoVisibility(video.id)}
+            className="cursor-pointer p-4 text-center flex justify-between items-center gap-10"
+          >
+            <Image src={video.src} alt={video.alt} width={300} />
+            <div className="flex justify-between w-full">
+              <article>
+                <p>{video.title}</p>
+                <p>{video.content}</p>
+              </article>
+              {visibleVideoId === video.id ? (
+                <div>
+                  <ChevronUpIcon className="w-6 h-6" />
+                </div>
+              ) : (
+                <div>
+                  <ChevronDownIcon className="w-6 h-6" />
+                </div>
+              )}
+            </div>
+          </section>
+          {visibleVideoId === video.id && (
+            <div className="video-container flex justify-center mt-4">
+              <iframe
+                width="1000"
+                height="515"
+                src={video.videoUrl}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          )}
         </div>
-      )}
+      ))}
     </div>
   );
 };
